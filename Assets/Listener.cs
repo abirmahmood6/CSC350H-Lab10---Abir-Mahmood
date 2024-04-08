e0db3e019adc7eeea36372bc71eb722ebf10d376
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2024-04-07 23:27:16 -0400

    lab10

---
+++ Assets/Listener.cs
@@ -0,0 +1,28 @@
+// Abir Mahmood
+// CSC-350H
+// Dr. Hao Tang
+// Listener.cs
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour
+{
+    private Invoker invoker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get access to the Invoker class
+        invoker = Camera.main.GetComponent<Invoker>();
+
+        // Add the method as a listener
+        invoker.AddNoArgumentListener(Message);
+    }
+
+    private void Message()
+    {
+        //print the message in console
+        Debug.Log("Message!");
+    }
+}
---
+++ Assets/MessageEvent.cs
@@ -0,0 +1,14 @@
+// Abir Mahmood
+// CSC-350H
+// Dr. Hao Tang
+// MessageEvent.cs
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MessageEvent : UnityEvent
+{
+
+}
